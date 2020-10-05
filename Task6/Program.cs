0515bb6eeb7b8b5ea941fe85030fe610dd89efca
Евгений Русин <mn-161@mail.ru>
2020-10-05 11:59:55 +0500

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        public static int SumNum(int a)
+        {
+            int result = 0;
+            while (a != 0)
+            {
+                result += a % 10;
+                a /= 10;
+            }
+            
+            return result;
+
+
+        }
+    
+    
+        static void Main(string[] args)
+        {
+
+            //Написать программу подсчета количества «Хороших» чисел в диапазоне от 1 до 1 000 000 000.
+            //Хорошим называется число, которое делится на сумму своих цифр.
+            //Реализовать подсчет времени выполнения программы, используя структуру DateTime.
+            DateTime date1 = DateTime.Now;
+            Console.WriteLine(date1.ToLongTimeString());
+            int answer = 0;
+            for (int i = 1; i <= 1000000000; i++)
+            {
+                if(i%SumNum(i) == 0)
+                {
+                    answer += i;
+                }
+            }
+            
+            Console.WriteLine(answer);
+            DateTime date2 = DateTime.Now;
+            Console.WriteLine(date2.ToLongTimeString());            
+            Console.WriteLine($"Время выполнения: {date2.Subtract(date1)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void NumInterval(int a, int b)
+        {
+            a++;
+            Console.WriteLine(a);
+            if(b-1>a)
+            {
+                NumInterval(a, b);
+            }
+                        
+        }
+        static int NumSum(int a, int b)
+        {
+
+            int result = a;            
+            a++;
+            if(a < b+1)
+            {
+                result += NumSum(a, b);
+            }
+            
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            //a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a < b);
+            //б) *Разработать рекурсивный метод, который считает сумму чисел от a до b.
+
+            int a, b;
+            Console.Write("Enter the a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter the b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+            NumInterval(a, b);
+            Console.WriteLine(b);
+            Console.WriteLine($"Sum numbers between a and b: {NumSum(a,b)}");
+            Console.ReadKey();
+
+        }
+    }
+}
